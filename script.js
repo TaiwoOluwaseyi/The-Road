f6b276cbb14fc43e126f2e200d9f5a6efdbe7faf
Mabel Taiwo <seyitaiwo21@gmail.com>
2022-10-03 13:32:30 +0100

    another commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const colors = ['#6495ed', '#7ffd4', '#ffa07a', '#f08080', '#afeeee'];
+
+let i = 0;
+
+Array.from(document.querySelectorAll('.nav-link')).forEach(item => {
+ item.style.cssText = `background-color: ${colors[i++]}`
+})
+
+
+
+
+
 Array.from(document.querySelectorAll('.navigation-button')).forEach(item => {
   item.onclick = () => {
     item.parentElement.parentElement.classList.toggle('change')
